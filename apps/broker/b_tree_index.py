88bce2dc6ddccb0bcbf717f25d4b821b4c642b85
Daniel Fąderski <faderskd@users.noreply.github.com>
2023-10-21 11:30:13 +0200

    deleting node

--- apps/broker/b_tree_index.py
+++ apps/broker/b_tree_index.py
@@ -2,79 +2,129 @@ import typing
 from collections import defaultdict
 from dataclasses import dataclass
 
-MAX_KEYS = 3
-
 
 @dataclass
 class DeleteResult:
     new_first: typing.Optional[int]
+    not_enough_keys: bool = False
+    leaf: bool = False
 
 
 class BTreeNode:
-    def __init__(self):
+    def __init__(self, max_keys: int):
         self.children: typing.List[BTreeNode] = []
         self.keys: typing.List[int] = []
+        self.values: typing.Optional[typing.List[str]] = None  # None in non-leaf nodes
+        self.max_keys = max_keys
 
     def insert(self, key: int, value: str) -> typing.Optional['BTreeNode']:
         for i in range(len(self.keys)):
             if self.keys[i] >= key:
                 maybe_new_node = self.children[i].insert(key, value)
+                insert_index = i
                 break
         else:
             maybe_new_node = self.children[-1].insert(key, value)
+            insert_index = len(self.children)
 
         if maybe_new_node:
             first_key = maybe_new_node.keys[0]
-            for i in range(len(self.keys)):
-                if self.keys[i] >= first_key:
-                    self.keys.insert(i, first_key)
-                    self.children = self.children[:i] + maybe_new_node.children + self.children[i + 1:]
-                    break
+            self.keys.insert(insert_index, first_key)
+            if insert_index < len(self.children):
+                self.children = self.children[:insert_index] + maybe_new_node.children + self.children[
+                                                                                         insert_index + 1:]
             else:
-                self.keys.append(first_key)
                 self.children.pop()
                 self.children.extend(maybe_new_node.children)
 
-        if len(self.keys) > MAX_KEYS:
-            mid = (len(self.keys) + 1) // 2
+        if len(self.keys) > self.max_keys:
+            mid = len(self.keys) // 2
             child_mid = (len(self.children) + 1) // 2
             left_keys, right_keys = self.keys[:mid], self.keys[mid + 1:]
             left_children, right_children = self.children[:child_mid], self.children[child_mid:]
-            left_child = BTreeNode()
+            left_child = BTreeNode(self.max_keys)
             left_child.keys = left_keys
             left_child.children = left_children
-            right_child = BTreeNode()
+            right_child = BTreeNode(self.max_keys)
             right_child.keys = right_keys
             right_child.children = right_children
-            parent = BTreeNode()
+            parent = BTreeNode(self.max_keys)
             parent.keys = [self.keys[mid]]
             parent.children = [left_child, right_child]
             return parent
 
     def delete(self, key: int) -> typing.Optional[DeleteResult]:
+        # search for key to delete
         for i in range(len(self.keys)):
             if self.keys[i] > key:
                 delete_res = self.children[i].delete(key)
-                if delete_res:
-                    if i > 0 and self.keys[i - 1] == key:
-                        self.keys[i - 1] = delete_res.new_first
-                    break
+                break
         else:
-            delete_res = self.children[-1].delete(key)
-            if delete_res and self.keys[-1] == key:
-                self.keys[-1] = delete_res.new_first
+            i = len(self.keys)
+            delete_res = self.children[i].delete(key)
 
-        # TODO: handle rebalance
+        if not delete_res:
+            return
+
+        # we are parent, child has not enough keys, so try to borrow from siblings
+        if delete_res.leaf and delete_res.not_enough_keys:
+            if i > 0 and self.children[i - 1]._has_enough_to_lend():
+                # borrow right-most key from left child
+                self.children[i].keys.insert(0, self.children[i - 1].keys.pop())
+                self.children[i].values.insert(0, self.children[i - 1].values.pop())
+            elif i + 1 < len(self.children) and self.children[i + 1]._has_enough_to_lend():
+                # borrow left-most key from right child
+                self.children[i].keys.append(self.children[i + 1].keys.pop(0))
+                self.children[i].values.append(self.children[i + 1].values.pop(0))
+            else:
+                # we have empty child !
+                pass
+
+        # we are parent, we deleted from leaf and try to remove any empty child
+        if delete_res.leaf:
+            self.children = [c for c in self.children if c.keys]
+            self.keys = []
+            # rearrange our keys
+            for i in range(1, len(self.children)):
+                self.keys.append(self.children[i].keys[0])
+            if self.children:
+                delete_res.new_first = self.children[0].keys[0]
+
+        # we are parent, we deleted from leaf, tried to borrow, but it didn't help, we rearranged the keys and it is still bad
+        if not self._is_at_least_half_full():
+            delete_res.not_enough_keys = True
+        # condition of the b+tree is maintained
+        else:
+            delete_res.not_enough_keys = False
+
+
+
+        # we deleted from leaf, we are not a parent, we have to replace deleted element (if present) with the inorder successor
+        if not delete_res.leaf and not delete_res.not_enough_keys:
+            for i in range(len(self.keys)):
+                if self.keys[i] == key:
+                    self.keys[i] = delete_res.new_first
+
+
+        delete_res.leaf = False
         return delete_res
+
+    def _has_enough_to_lend(self):
+        return len(self.keys) > self.max_keys // 2
+
+    def _is_at_least_half_full(self):
+        return len(self.keys) >= self.max_keys // 2 and len(self.children) > self.max_keys // 2
 
     def __repr__(self):
         return str(self.keys)
 
 
 class BTreeNodeLeaf(BTreeNode):
-    def __init__(self):
-        super().__init__()
-        self.values: typing.List[str] = []
+    def __init__(self, max_keys: int):
+        super().__init__(max_keys)
+        self.next: typing.Optional[BTreeNode] = None
+        self.prev: typing.Optional[BTreeNode] = None
+        self.values = []
 
     def insert(self, key: int, value: str) -> typing.Optional[BTreeNode]:
         for i in range(len(self.keys)):
@@ -86,17 +136,27 @@ class BTreeNodeLeaf(BTreeNode):
             self.keys.append(key)
             self.values.append(value)
 
-        if len(self.keys) > MAX_KEYS:
-            mid = (len(self.keys) + 1) // 2
+        if len(self.keys) > self.max_keys:
+            mid = len(self.keys) // 2
             left_keys, right_keys = self.keys[:mid], self.keys[mid:]
             left_values, right_values = self.values[:mid], self.values[mid:]
-            left_child = BTreeNodeLeaf()
+            left_child = BTreeNodeLeaf(self.max_keys)
             left_child.keys = left_keys
             left_child.values = left_values
-            right_child = BTreeNodeLeaf()
+            right_child = BTreeNodeLeaf(self.max_keys)
             right_child.keys = right_keys
             right_child.values = right_values
-            parent = BTreeNode()
+
+            # left_child.next = right_child
+            # left_child.prev = self.prev
+            # right_child.prev = left_child
+            # right_child.next = self.next
+            # if self.prev:
+            #     self.prev.next = left_child
+            # if self.next:
+            #     self.next.prev = right_child
+
+            parent = BTreeNode(self.max_keys)
             parent.keys = [self.keys[mid]]
             parent.children = [left_child, right_child]
             return parent
@@ -109,18 +169,23 @@ class BTreeNodeLeaf(BTreeNode):
         else:
             raise NoSuchKeyException(f'No key {key} found in a tree')
 
-        half_full = len(self.keys) >= MAX_KEYS // 2
-        if not half_full:
-            pass  # TODO: handle rebalance
-        return DeleteResult(self.keys[0])
+        if self._is_at_least_half_full():
+            # case when after deletion b+tree condition is maintained in leaf, nothing to do more
+            return DeleteResult(self.keys[0], leaf=True)
+        # case when there is not enough elements in leaf after deletion
+        if self.keys:
+            return DeleteResult(self.keys[0], not_enough_keys=True, leaf=True)
+        return DeleteResult(None, not_enough_keys=True, leaf=True)
 
     def __repr__(self):
         return str(self.keys)
 
+    def _is_at_least_half_full(self):
+        return len(self.keys) >= self.max_keys // 2
 
 class BTree:
-    def __init__(self):
-        self.root = BTreeNodeLeaf()
+    def __init__(self, max_keys: int):
+        self.root = BTreeNodeLeaf(max_keys)
         self.height = 1
 
     def insert(self, key: int, value: str):
@@ -140,6 +205,16 @@ class BTree:
                 node_keys = "|".join(map(str, node.keys))
                 print(f'[{node_keys}]', end=' ')
             print()
+
+    def print_leafs(self):
+        curr_node = self.root
+        while curr_node.children:
+            curr_node = curr_node.children[0]
+        assert isinstance(curr_node, BTreeNodeLeaf)
+        while curr_node:
+            node_keys = "|".join(map(str, curr_node.keys))
+            print(f'[{node_keys}]', end='->')
+            curr_node = curr_node.next
 
     def _dfs(self, root: BTreeNode, level: int, container: typing.Dict[int, typing.List[BTreeNode]]):
         container[level].append(root)
--- tests/test_btree.py
+++ tests/test_btree.py
@@ -6,7 +6,7 @@ from apps.broker.b_tree_index import BTree, BTreeNode
 
 class TestBTree(unittest.TestCase):
     def setUp(self) -> None:
-        self.tree = BTree()
+        self.tree = BTree(3)
 
     def test_should_build_proper_tree(self):
         # given
@@ -32,7 +32,61 @@ class TestBTree(unittest.TestCase):
         self.tree.insert(0, "val20")
 
         # expect
-        self.assertEqual(self._dfs(self.tree), [2, 2, 6, 0, 1, 0, 0, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 5, 20, 6, 7, 10, 20, 100])
+        self.assertEqual(self._dfs(self.tree),
+                         [2, 2, 6, 0, 1, 0, 0, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 5, 20, 6, 7, 10, 20, 100])
+
+    def test_should_properly_delete_from_tree_case1(self):
+        # given
+        self.tree.insert(10, "val1")
+        self.tree.insert(29, "val2")
+        self.tree.insert(40, "val3")
+        self.tree.insert(25, "val4")
+        self.tree.insert(0, "val5")
+        self.tree.insert(5, "val6")
+        self.tree.insert(60, "val7")
+        self.tree.insert(2, "val8")
+        self.tree.insert(15, "val9")
+        self.tree.insert(16, "val10")
+        self.tree.insert(1, "val11")
+
+        # expect
+        self.assertEqual(self._dfs(self.tree), [16, 2, 10, 0, 1, 2, 5, 10, 15, 29, 16, 25, 29, 40, 60])
+
+        # when
+        self.tree.delete(5)
+        self.tree.delete(2)
+        self.tree.delete(1)
+        self.tree.delete(16)
+        self.tree.delete(29)
+        self.tree.delete(25)
+
+        # then
+        self.assertEqual(self._dfs(self.tree), [40, 10, 15, 0, 10, 15, 60, 40, 60])
+
+    def test_should_properly_delete_from_tree_case2(self):
+        # given
+        self.tree.insert(10, "val1")
+        self.tree.insert(29, "val2")
+        self.tree.insert(40, "val3")
+        self.tree.insert(25, "val4")
+        self.tree.insert(0, "val5")
+        self.tree.insert(5, "val6")
+        self.tree.insert(60, "val7")
+        self.tree.insert(2, "val8")
+        self.tree.insert(15, "val9")
+        self.tree.insert(16, "val10")
+        self.tree.insert(1, "val11")
+
+        # expect
+        self.assertEqual(self._dfs(self.tree), [16, 2, 10, 0, 1, 2, 5, 10, 15, 29, 16, 25, 29, 40, 60])
+
+        # when
+        self.tree.delete(40)
+        self.tree.delete(60)
+        self.tree.delete(29)
+
+        # then
+        self.assertEqual(self._dfs(self.tree), [16, 2, 10, 0, 1, 2, 5, 10, 15, 25, 16, 25])
 
     def _dfs(self, tree: BTree) -> typing.List[int]:
         dfs_container = []
